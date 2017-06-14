18b9ba2ef23f7f893cde91bb09d5c9acbd766daf
Pavel Daleuski <clubstar@yandex.ru>
2017-06-14 17:30:34 +0300

    scheduler: initial structure

---
+++ dhtmlx_scheduler.js
@@ -0,0 +1,16 @@
+;
+gantt.scheduler = {
+    calendar: {
+        tasks: {},
+        resource: {},
+        project: {
+            dates: {
+                0: false,
+                6: false
+            },
+            hours: [8, 17]
+        }
+    },
+    API1: function(){},
+    API2: function(){}
+};
